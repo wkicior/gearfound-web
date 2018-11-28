b6995d31eac969e7ec6d6e4fa665974a5a388e50
Wojciech Kicior <Wojciech.Kicior@gmail.com>
2018-11-28 22:31:21 +0100

    styling items list

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatInputModule} from "@angular/material";
+import {MatButtonModule, MatExpansionModule, MatInputModule} from "@angular/material";
 
 
 @NgModule({
-  imports: [MatButtonModule, MatInputModule],
-  exports: [MatButtonModule, MatInputModule],
+  imports: [MatButtonModule, MatInputModule, MatExpansionModule],
+  exports: [MatButtonModule, MatInputModule, MatExpansionModule],
 })
 export class MaterialModule { }
